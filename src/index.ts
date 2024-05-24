563ffa93f26e370993ac2d8a791cc80b7bb82c1b
AugustHD <augu0577@stud.kea.dk>
2024-05-24 09:26:36 +0200

    created consumer for order-fail

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import sendMail from './services/email.js';
 import { consumeUserCreated } from './messages/users.js';
-import { consumeOrderStarted } from './messages/orders.js';
+import { consumeOrderStarted, consumeOrderFailed } from './messages/orders.js';
 import { consumeShipmentSent } from './messages/shipment.js';
 
 await consumeUserCreated(sendMail);
 await consumeOrderStarted(sendMail);
+await consumeOrderFailed(sendMail);
 await consumeShipmentSent(sendMail);
+
 
